3f560a7b36b021e3adcd8b8cf47ae1dd68b011d4
Christopher Kapic <christopherkapic@gmail.com>
2021-07-22 19:24:41 -0500

    initial function

---
+++ functions/forks.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+const GITHUB_KEY = process.env.GITHUB_KEY;
+const UPSTASH_PORT = process.env.UPSTASH_PORT;
+const UPSTASH_UN = process.env.UPSTASH_UN;
+const UPSTASH_PW = process.env.UPSTASH_PW;
+
+
+exports.handler = async({ body, headers }, context) => {
+    const user = body.user
+    const repo = body.repo
+    const repo_res = await fetch(`https://api.github.com/repos/${user}/${repo}/forks`)
+    const repo_json = await repo_res.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(repo_json)
+    }
+
+}
