171cf582565873624684e65061965fe0e58e17f6
Christopher Kapic <christopherkapic@gmail.com>
2021-07-22 21:30:56 -0500

    please work online

--- functions/forks.js
+++ functions/forks.js
@@ -8,27 +8,36 @@ const UPSTASH_UN = process.env.UPSTASH_UN;
 const UPSTASH_PW = process.env.UPSTASH_PW;
 
 
-exports.handler = async(event, context) => {
+exports.handler = async (event, context) => {
     const body = JSON.parse(event.body)
     const user = body.user
     const repo = body.repo
 
-    // const repo_res = await fetch(`https://api.github.com/repos/${user}/${repo}/forks`)
-    const repo_json = await octokit.request("GET /repos/{owner}/{repo}", {
+    const repo_json = await octokit.request("GET /repos/{owner}/{repo}/forks", {
         owner: user,
         repo: repo,
     })
-    // const repo_json = await repo_res.json();
-    console.log(repo_json)
 
-    let forks = []
+    var forks = []
 
-    repo_json.forEach((fork) => {
-        const fork_res = await fetch(`https://api.github.com/repos/${pulluser}/Stocksera/compare/${headuser}:master...master`)
-        const fork_json = await fork_res.json();
-        forks.push(fork_json)
+    repo_json.data.forEach(async (fork) => {
+        console.log('here')
+        // fetch(`https://api.github.com/repos/${fork.owner.login}/Stocksera/compare/${user}:master...master`)
+            // .then(res => res.json())
+        octokit.request(`GET /repos/${fork.owner.login}/Stocksera/compare/${user}:master...master`)
+            .then(json => {
+                forks.push(json)
+            })
+        // const fork_json = await fork_res.json();
+        // forks.push(fork_json)
     })
 
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //         forks: forks
+    //     })
+    // }
     return {
         statusCode: 200,
         body: JSON.stringify({
