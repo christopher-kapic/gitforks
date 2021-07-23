5a3b1fcb0b174d58ab23309db2806c7182d21883
Christopher Kapic <christopherkapic@gmail.com>
2021-07-22 22:04:18 -0500

    it'll work now, just you wait

--- functions/forks.js
+++ functions/forks.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const { Octokit, App, Action } = require("octokit");
 const GITHUB_KEY = process.env.GITHUB_KEY;
+const GITHUB_KEY_V2 = process.env.GITHUB_KEY_V2;
 const octokit = new Octokit({ auth: GITHUB_KEY })
 
 const UPSTASH_PORT = process.env.UPSTASH_PORT;
@@ -19,13 +20,24 @@ exports.handler = async (event, context) => {
     })
 
     let forks = repo_json.data.map((fork) => {
-        return(octokit.request(`GET /repos/{fullname}/compare/{user}:master...master`, {
-            fullname: fork.full_name,
-            user: user
+        // return(octokit.request(`GET /repos/{fullname}/compare/{user}:master...master`, {
+        //     fullname: fork.full_name,
+        //     user: user
+        // }))
+        return(fetch(`https://api.github.com/repos/${fork.full_name}/compare/${user}:master...master`, {
+            headers: {
+                authorization: `Basic ${GITHUB_KEY_V2}`
+            }
         }))
     })
 
     forks = await Promise.all(forks)
+
+    let forks_json = forks.map((fork) => {
+        return(fork.json())
+    })
+
+    forks_json = await Promise.all(forks_json)
 
     // repo_json.data.forEach(async (fork) => {
     //     console.log('here')
@@ -41,7 +53,7 @@ exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            forks: forks
+            forks: fork_json
         })
     }
     // return {
