97e5aac1a82066101fa9295bef22c49ecf32292e
Christopher Kapic <christopherkapic@gmail.com>
2021-07-24 21:24:58 -0500

    markdown?

--- src/components/repo/Readme.js
+++ src/components/repo/Readme.js
@@ -1,8 +1,18 @@
+import ReactMarkdown from 'react-markdown'
+import { useState, useEffect } from 'react'
+import fetch from 'node-fetch'
 
 
 const Readme = (props) => {
+    const [markdown, setMarkdown] = useState(false)
 
-    return(<><p>{props.readme}</p></>)
+    useEffect(() => {
+        fetch(props.readme).then(res => res.text).then(md => setMarkdown(md))
+    }, [])
+
+    return(
+        <ReactMarkdown remarkPlugins={[gfm]} rehypePlugins={[raw]} children={ markdown }/>
+    )
 }
 
 export default Readme
