c26cb0c82878e9aef3ab3588e6c35e1bae65c9de
Christopher Kapic <christopherkapic@gmail.com>
2021-07-27 14:35:02 +0000

    readme images

--- src/components/repo/Readme.js
+++ src/components/repo/Readme.js
@@ -12,11 +12,34 @@ const styles = {
     }
 }
 
+/**
+ * @param {String} markdown Original markdown from Github
+ * @returns {String} Markdown, but image links are updated for raw.githubusercontent
+ */
+const updateImageLinks = (md, rm) => {
+    const reg = /^!\[[^\]]*]\(\.\/([a-zA-Z]+(\/[a-zA-Z]+)+)([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?[a-zA-Z]+\)$/gm
+
+    let to_return = md;
+
+    const to_replace = md.match(reg);
+
+    to_replace.forEach((img) => {
+        let new_img = img.split(')[./')[1]
+        new_img = new_img.substring(0, new_img.length - 1);
+        const repo = rm.substring(0, rm.length - 10)
+        new_img = `${repo}/${new_img}`
+        to_return.replace(img, new_img)
+    })
+
+
+    return (to_return)
+}
+
 const Readme = (props) => {
     const [markdown, setMarkdown] = useState('')
 
     useEffect(() => {
-        fetch(props.readme).then(res => res.text()).then(md => setMarkdown(md))
+        fetch(props.readme).then(res => res.text()).then(md => setMarkdown(updateImageLinks(md, props.readme)))
     }, [props.readme])
 
     return(
