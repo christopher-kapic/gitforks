10f50c4a96650f63ca342cd92ea7b85fd0cd8861
Christopher Kapic <christopherkapic@gmail.com>
2021-07-27 14:53:36 +0000

    markdown

--- src/components/repo/Readme.js
+++ src/components/repo/Readme.js
@@ -49,7 +49,7 @@ const Readme = (props) => {
 
     useEffect(() => {
         fetch(props.readme).then(res => res.text()).then(md => setMarkdown(updateImageLinks(md, props.readme)))
-        console.log(updateImageLinks(md, props.readme))
+        console.log(updateImageLinks(markdown, props.readme))
     }, [props.readme, markdown])
 
     return(
