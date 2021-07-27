21b3abb5adda2b26b2fe71f54a9d70d023780613
Christopher Kapic <christopherkapic@gmail.com>
2021-07-27 14:44:11 +0000

    boom

--- src/components/repo/Readme.js
+++ src/components/repo/Readme.js
@@ -28,9 +28,7 @@ const updateImageLinks = (md, rm) => {
     }
 
     to_replace.forEach((img) => {
-        let new_img = img.split(')[./')[1]
-        console.log(img)
-        console.log(new_img)
+        let new_img = img.split('](./')[1]
         new_img = new_img.substring(0, new_img.length - 1);
         const repo = rm.substring(0, rm.length - 10)
         new_img = `${repo}/${new_img}`
